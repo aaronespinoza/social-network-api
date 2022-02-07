942c960ceeb525fa7d5f7ca5edab7dfafdd5f155
aaronespinoza <aarone@berkeley.edu>
2022-02-07 11:37:29 -0800

    added user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./thought");
+
+const reactionSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: " ",
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
