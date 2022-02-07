cc3100b8e5f6b9b3da68e28bd3ab6adcc590ff16
aaronespinoza <aarone@berkeley.edu>
2022-02-07 11:50:19 -0800

    do a little test a little

---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./reaction");
+const moment = require("moment");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: () => moment().format("LLL"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reaction.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
