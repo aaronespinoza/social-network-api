4e7ccf2bf4636d262f5fdfdc0d82a4ab61401d65
aaronespinoza <aarone@berkeley.edu>
2022-02-07 21:42:38 -0800

    some controler

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,21 @@
 const { Thought, User } = require("../models");
 const reactionSchema = require("../models/reaction");
 
+module.exports = {
+
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought matches that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
