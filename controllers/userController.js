80f03b2a77595c5ac3d975bfd4c48023c36f9f58
aaronespinoza <aarone@berkeley.edu>
2022-02-08 14:34:30 -0800

    changed controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Thought, Reaction } = require("../models");
+const { User } = require("../models");
 
 module.exports = {
 
@@ -60,4 +60,31 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  addFriend(req,res){
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: {friends:req.params.friendId}},
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No user matches this id!' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  },
+  removeFriend(res,req){
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: {friends:req.params.friendId}},
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No user matches this id!' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  }
+
 };
